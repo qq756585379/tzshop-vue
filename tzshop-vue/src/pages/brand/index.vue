af682c42ce9e5d363a2d15256403ca3c58699700
杨俊 <756585379@qq.com>
2020-04-28 15:41:58 +0800

    no message

--- tzshop-vue/src/pages/brand/index.vue
+++ tzshop-vue/src/pages/brand/index.vue
@@ -30,7 +30,7 @@
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
-  import api from '../../js/http';
+  import api from '../../js/api';
 
   export default {
     data() {
